be99aaf0d3dd434ac8ae2d72d502171e1183c5e5
Edward Mike <ed.jmike@gmail.com>
2018-05-07 15:14:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     "Fizzbuzz"
-  elsif int % 5 == 0
+  elsif number % 5 == 0
     "Buzz"
-  elsif int % 3 == 0
+  elsif number % 3 == 0
     "Fizz"
   end
 end
